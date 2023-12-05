cbd716838ebcbd6c599af09098a486a8d3f76c6e
Max Willian <87044552+MaxWillianm@users.noreply.github.com>
2023-12-04 23:30:17 -0300

    add rota da api

--- src/routes/email.jsx
+++ src/routes/email.jsx
@@ -2,42 +2,45 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function Email() {
-  const [email, setEmail] = useState([
-    'email', ''
-  ]);
+  const [email, setEmail] = useState('');
   const [carregando, setCarregando] = useState(false);
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const url = '';
+  const [resultado, setResultado] = useState('');
+  const url = 'https://api-flask-ml.fly.dev/classify';
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setCarregando(true);
     try {
-      const data = JSON.stringify(email);
+      const data = JSON.stringify({ text: email });
       const response = await axios.post(url, data, {});
       console.log(response.data);
+      setResultado(response.data);
       setIsFormSubmitted(true);
-    }
-    catch (error) {
+    } catch (error) {
       console.error(error);
     }
     setCarregando(false);
   };
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
-    setEmail(prevEmail => ({ ...prevEmail, [name]: value }));
+    const { value } = event.target;
+    setEmail(value);
   };
 
   return (
     <div className='container'>
       <div className="bg-black-div/90 border border-white rounded-xl  my-20 py-12 px-8">
         <form id='formProduto' className='flex flex-col space-y-12' onSubmit={handleSubmit}>
-          <textarea className='bg-black-input text-white rounded px-4 py-3' placeholder='Corpo do email' type="text" name="email" value={email.email} onChange={handleChange} />
+          <textarea className='bg-black-input text-white rounded px-4 py-3' placeholder='Corpo do email' type="text" value={email} onChange={handleChange} />
           <button className='text-white flex w-1/4 mx-auto justify-center items-center bg-blue-high rounded p-5 transition-all hover:bg-blue-light' type="submit" disabled={carregando}>
             {carregando ? <img src="../../public/svg/loading_white.svg" className='w-9 h-9' /> : 'Enviar'}
           </button>
-          {isFormSubmitted && <p className='text-white'>Enviado!</p>}
+          {isFormSubmitted && (
+            <div>
+              <p className='text-white text-center text-4xl'>Resultado: {resultado.spam == true ? 'É spam' : 'Não é spam'}</p>
+            </div>
+          )}
         </form>
       </div>
     </div>
